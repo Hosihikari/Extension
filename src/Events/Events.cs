fb21cf4e0adf610e3c67b5112b41746eef84d92b
lazulikao <gaoxinhong2004@hotmail.com>
2023-07-26 16:15:52 +0800

    fix: add left event instance

--- src/Events/Events.cs
+++ src/Events/Events.cs
@@ -9,7 +9,9 @@ public static class Events
 
     private static readonly Lazy<InitializedEvent> PlayerInitializedEvent = new(() => new());
     public static InitializedEvent PlayerInitialized => PlayerInitializedEvent.Value;
-    
+
     private static readonly Lazy<JoinEvent> PlayerJoinEvent = new(() => new());
     public static JoinEvent PlayerJoin => PlayerJoinEvent.Value;
+    private static readonly Lazy<LeftEvent> PlayerLeftEvent = new(() => new());
+    public static LeftEvent PlayerLeft => PlayerLeftEvent.Value;
 }
