43a45b938d2b9c89a32a721910278b32213d5514
LazuliKao <lazulikao@nbt.edu.cn>
2023-07-30 17:19:19 +0800

    fix: ouput log to console

--- src/Events/Implements/Log.cs
+++ src/Events/Implements/Log.cs
@@ -4,5 +4,11 @@ namespace Hosihikari.Minecraft.Extension.Events.Implements;
 
 public class Log
 {
-    public static Logger Logger { get; } = new("Events");
+    private static readonly Lazy<Logger> _logger = new(() =>
+    {
+        var instance = new Logger(nameof(Events));
+        instance.SetupConsole();
+        return instance;
+    });
+    public static Logger Logger => _logger.Value;
 }
--- src/GlobalService/Log.cs
+++ src/GlobalService/Log.cs
@@ -4,5 +4,11 @@ namespace Hosihikari.Minecraft.Extension.GlobalService;
 
 public static class Log
 {
-    public static Logger Logger { get; } = new("GlobalService");
+    private static readonly Lazy<Logger> _logger = new(() =>
+    {
+        var instance = new Logger(nameof(GlobalService));
+        instance.SetupConsole();
+        return instance;
+    });
+    public static Logger Logger => _logger.Value;
 }
